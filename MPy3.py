b375ccef49c0cfcf97d36a8520bcdd6c0bb2ff2a
Tobias A. Rau <github@tar-it.com>
2023-12-02 23:55:11 +0100

    Implement meta.py

--- MPy3.py
+++ MPy3.py
@@ -1,5 +1,5 @@
 # MPTube3 - a YouTube-to-mp3-converter
-from lib import ffmpeg
+from lib import ffmpeg, meta
 from pytube import YouTube
 import subprocess
 import sys
@@ -65,7 +65,7 @@ def download_and_convert_to_mp3(url, download_location):
 
     # Get the best available video stream
     print("Getting highest bitrate stream available...")
-    video_stream = video.streams.get_audio_only()
+    stream = video.streams.get_audio_only()
 
     # Clean the video title to remove invalid characters for use as a file name
     cleaned_title = clean_filename(video.title)
@@ -74,23 +74,34 @@ def download_and_convert_to_mp3(url, download_location):
     # Download video to the specified location
     print(f"Downloading file to '{download_location}'...")
     os.makedirs(download_location, exist_ok=True)
-    video_file_path = os.path.join(download_location, f"{cleaned_title}.mp4")
-    video_stream.download(output_path=download_location, filename=f"{cleaned_title}.mp4")
+    file_path = os.path.join(download_location, f"{cleaned_title}.mp4")
+    stream.download(output_path=download_location, filename=f"{cleaned_title}.mp4")
 
     # Wait until the file is fully downloaded
-    while not os.path.exists(video_file_path):
+    while not os.path.exists(file_path):
         pass
-    print(f"The file has been downloaded successfully to {video_file_path}.")
+    print(f"The file has been downloaded successfully to {file_path}.")
 
     # Convert video to MP3 using FFmpeg
     print(f"Converting file to MP3 using FFmpeg...")
-    subprocess.run(['ffmpeg', '-i', video_file_path, '-q:a', '0', '-map', 'a', video_file_path.replace('.mp4', '.mp3')],
+    subprocess.run(['ffmpeg', '-i', file_path, '-q:a', '0', '-map', 'a', file_path.replace('.mp4', '.mp3')],
                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print(f"The file has been converted to MP3 successfully: {video_file_path.replace('.mp4', '.mp3')}.")
+    print(f"The file has been converted to MP3 successfully: {file_path.replace('.mp4', '.mp3')}.")
+    
+        # Fetch Discogs metadata
+    discogs_metadata = meta.get_meta(cleaned_title)
 
+    if discogs_metadata:
+        print("\nDiscogs Metadata:")
+        for key, value in discogs_metadata.items():
+            print(f"{key}: {value}")
+
+        # Attach Discogs metadata to the downloaded file
+        meta.attach_meta(file_path, discogs_metadata)
+    
     # Delete the original video file if needed
     print(f"Deleting old file...")
-    os.remove(video_file_path)
+    os.remove(file_path)
     print(f"Done!")
 
 
--- lib/meta.py
+++ lib/meta.py
@@ -1,1 +1,45 @@
-# script for fetching meta data and saving it to the file
+# script for fetching metadata using the Discogs API
+import discogs_client
+import os
+from mutagen.id3 import ID3, TIT2, TPE1, TDRC
+
+def get_meta(title):
+    try:
+        # Access the environment variable - or set user token manually
+        user_token = os.environ.get("DISCOGS_USER_TOKEN")
+
+        # Initialize Discogs client with the user token
+        discogs = discogs_client.Client('MPy3.py', user_token=user_token)
+
+        # Search for releases based on the video title
+        search_results = discogs.search(title, type='release')
+
+        if search_results:
+            # Take the first result
+            release = search_results[0]
+
+            metadata = {
+                'title': release.title,
+                'artist': release.artists[0].name if release.artists else '',
+                'year': release.year,
+                # TODO: Add more meta tags
+            }
+            return metadata
+        else:
+            print("No Discogs results found.")
+            return None
+    except Exception as e:
+        print(f"Error fetching Discogs metadata: {e}")
+        return None
+
+def attach_meta(file_path, metadata):
+    try:
+        audio = ID3(file_path.replace('.mp4', '.mp3'))
+        audio.add(TIT2(encoding=3, text=metadata['title']))
+        audio.add(TPE1(encoding=3, text=metadata['artist']))
+        audio.add(TDRC(encoding=3, text=str(metadata['year'])))
+        # TODO: Add more meta tags
+        audio.save()
+        print("Metadata attached to the file.")
+    except Exception as e:
+        print(f"Error attaching metadata to the file: {e}")
